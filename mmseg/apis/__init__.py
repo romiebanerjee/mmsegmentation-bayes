62f21f3f523fc361a23dc84b613f70c269f15afc
romiebanerjee <romie.banerjee@gmail.com>
2025-08-14 17:31:28 +0200

    update mmseg/apis/__init__.py

--- mmseg/apis/__init__.py
+++ mmseg/apis/__init__.py
@@ -3,7 +3,7 @@ from .inference import inference_model, init_model, show_result_pyplot
 from .mmseg_inferencer import MMSegInferencer
 from .remote_sense_inferencer import RSImage, RSInferencer
 from .mcglm import MCGLM
-from .avu import probabilities
+from .avu import calculate_avu_metrics, find_optimal_thresholds, plot_avu_analysis
 
 __all__ = [
     'init_model', 'inference_model', 'show_result_pyplot', 'MMSegInferencer',
