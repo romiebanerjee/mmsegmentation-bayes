c5f069c824d5d0ab5e9d0b0ab09ef9b52e089a65
romiebanerjee <romie.banerjee@gmail.com>
2025-08-17 22:28:45 +0200

    SegGLM __init__

--- mmseg/apis/__init__.py
+++ mmseg/apis/__init__.py
@@ -2,7 +2,7 @@
 from .inference import inference_model, init_model, show_result_pyplot
 from .mmseg_inferencer import MMSegInferencer
 from .remote_sense_inferencer import RSImage, RSInferencer
-from .mcglm import SegGLM
+from .glm import SegGLM
 from .avu import calculate_avu_metrics, find_optimal_thresholds, plot_avu_analysis
 
 __all__ = [
